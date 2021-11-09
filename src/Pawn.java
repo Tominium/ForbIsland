52dc466de9d5f8a57877246b9b449a500787da29
slhscs305 <bokai0602@gmail.com>
2021-11-09 11:56:59 -0600

    Added pawn class and methods

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+
 public class Pawn {
+    private String role;
+    private ArrayList<Card> hand;
+    private int turnNum;
+
+    public Pawn(String r){
+        role = r;
+        turnNum = 0;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public ArrayList<Card> getDeck(){
+        return hand;
+    }
+
+    public int getTurnNum(){
+        return turnNum;
+    }
+
+    public void setDeck(ArrayList<Card> newDeck){
+        hand = newDeck;
+    }
+
+    public boolean addCard(Card c){
+        if(hand.size() >= 5){
+            return false;
+        }else{
+            hand.add(c);
+            return true;
+        }
+    }
+
+    public int compareTo(){
+
+    }
+
+    public void incrementTurnNum(){
+        turnNum++;
+    }
+
+    public void setTurnZero(){
+        turnNum = 0;
+    }
+
+
+
+
 }
