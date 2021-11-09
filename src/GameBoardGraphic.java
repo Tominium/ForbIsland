fd28159afa27c0d136db49994622aaff55965408
slhscs295 <siddhibavare@gmail.com>
2021-11-09 12:02:58 -0600

    import javax.swing.*;
    public class GameBoardGraphic {
        private static Integer WIDTH;
        private static Integer HEIGHT;
        private font Font;
        private JButton nextTurn;
        private JButton move;
        private JPanel gameBoard;
        private JPanel heliPanel;
        private JPanel specialAbility;
    
        public GameBoardGraphic(){}
    
        public void movePawn(){}
    
        public void tradeable(){}
    
        public void removeCard(){}
    
        public void sandBag(){}
    
        public void helicopter(){}
    
        public void specialAbility(){}
    
        public void winGraphics(){}
    
        public void loseGraphics(){}
    }

---
+++ src/GameBoardGraphic.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+public class GameBoardGraphic {
+    private static Integer WIDTH;
+    private static Integer HEIGHT;
+    private font Font;
+    private JButton nextTurn;
+    private JButton move;
+    private JPanel gameBoard;
+    private JPanel heliPanel;
+    private JPanel specialAbility;
+
+    public GameBoardGraphic(){}
+
+    public void movePawn(){}
+
+    public void tradeable(){}
+
+    public void removeCard(){}
+
+    public void sandBag(){}
+
+    public void helicopter(){}
+
+    public void specialAbility(){}
+
+    public void winGraphics(){}
+
+    public void loseGraphics(){}
+}
