632ec22737dd854dba8e798d03c8a881b66d2572
slhscs295 <siddhibavare@gmail.com>
2021-11-11 11:52:04 -0600

    added to gameboardgraphic it is not complete

--- src/Graphics/GameBoardGraphic.java
+++ src/Graphics/GameBoardGraphic.java
@@ -3,8 +3,8 @@ package Graphics;
 import javax.swing.*;
 import java.awt.*;
 public class GameBoardGraphic extends JFrame{
-    private static final int WIDTH = 1000;
-    private static final int HEIGHT = 1000;
+    private int WIDTH = 1000;
+    private int HEIGHT = 1000;
     private Font Font;
     private JButton nextTurn;
     private JButton move;
@@ -25,22 +25,32 @@ public class GameBoardGraphic extends JFrame{
         nextTurn.setLocation(150,150);
         move.setBounds(40,80,WIDTH, HEIGHT);
         move.setBackground(limeGreen);
-        move.setLocation(300,150)
+        move.setLocation(300,150);
+        gameBoard.add(nextTurn);
+        gameBoard.add(move);
+
     }
 
-    public void movePawn(){}
+    public void movePawn(){
+        //getHand();
+    }
 
-    public void tradeable(){}
+    public void tradeable(){
+
+    }
 
     public void removeCard(){}
 
-    public void sandBag(){}
+    public void sandBag(){
+    }
 
     public void helicopter(){}
 
     public void specialAbility(){}
 
-    public void winGraphics(){}
+    public void winGraphics(){
+
+    }
 
     public void loseGraphics(){}
 }
