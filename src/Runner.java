f4c2a5e3a875a8c911d67ebd3840cb71967c8c3b
Tomin <tchazhikat@gmail.com>
2021-11-10 18:50:45 -0600

    Commented a line

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,9 @@
 import Graphics.MainMenuFrame;
 
+
 public class Runner {
     public static void main(String args[]) {
+        //FlatGradiantoNatureGreenIJTheme.setup();
         MainMenuFrame graphicWindow = new MainMenuFrame();
     }
 }
