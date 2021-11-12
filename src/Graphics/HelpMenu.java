18e45af5bc0651ee71b2f451aa3356870dc04506
slhscs295 <siddhibavare@gmail.com>
2021-11-12 11:45:45 -0600

    updating something

--- src/Graphics/HelpMenu.java
+++ src/Graphics/HelpMenu.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public class HelpMenu extends JFrame {
     JLabel label = new JLabel();
+    private JFrame helpMenu;
     private JTextPane pane;
     private static int WIDTH = 800;
     private static int HEIGHT = 1000;
@@ -13,7 +14,7 @@ public class HelpMenu extends JFrame {
 
         Container win = getContentPane();
         win.setLayout(null);
-
+        helpMenu = new JFrame();
         JLabel heading = new JLabel("Help Menu");
         heading.setSize(750, 60);
         heading.setFont(new Font("Calibri", Font.BOLD, 50));
@@ -21,7 +22,7 @@ public class HelpMenu extends JFrame {
         heading.setHorizontalAlignment(JLabel.CENTER);
         heading.setVerticalAlignment(JLabel.CENTER);
         win.add(heading);
-
+        helpMenu.add(heading);
 
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(HIDE_ON_CLOSE);
