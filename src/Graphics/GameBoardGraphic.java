19e8b010cc5c5b6035fbfe32170b29dd9133da77
Tomin <tchazhikat@gmail.com>
2021-11-11 22:49:45 -0600

    Added tile images onto GridBagLayout

--- src/Graphics/GameBoardGraphic.java
+++ src/Graphics/GameBoardGraphic.java
@@ -1,12 +1,16 @@
 package Graphics;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
 
-public class GameBoardGraphic extends JFrame{
-    private static final int WIDTH = 1000;
-    private static final int HEIGHT = 1000;
+public class GameBoardGraphic extends JFrame implements MouseListener {
+    private static final int WIDTH = 1350;
+    private static final int HEIGHT = 750;
     private Font Font;
     private JButton nextTurn;
     private JButton move;
@@ -14,6 +18,8 @@ public class GameBoardGraphic extends JFrame{
     private JPanel heliPanel;
     private JPanel specialAbility;
     private Color limeGreen;
+    private final GridBagLayout GridBagLayoutgrid;
+    private GridBagConstraints gbc;
 
     public GameBoardGraphic(){
         nextTurn = new JButton("Next Turn");
@@ -28,7 +34,42 @@ public class GameBoardGraphic extends JFrame{
         move.setBounds(40,80,WIDTH, HEIGHT);
         move.setBackground(limeGreen);
         move.setLocation(300,150);
-        move.addActionListener((ActionListener) this);
+        addMouseListener(this);
+
+
+
+        GridBagLayoutgrid = new GridBagLayout();
+        gbc = new GridBagConstraints();
+        this.setLayout(GridBagLayoutgrid);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+//        GridBagLayout layout = new GridBagLayout();
+//        this.setLayout(layout);
+
+        File dir = new File("src/Assets/zones");
+
+        int x =2; int y=0; int i =0;
+        for(File f: dir.listFiles()){
+            if(!f.getName().contains("flood")){
+                try{
+                    gbc.gridx = x;
+                    gbc.gridy = y;
+                    Image image = ImageIO.read(f).getScaledInstance(100, 100,  java.awt.Image.SCALE_SMOOTH); // transform it
+                    this.add(new JLabel(new ImageIcon(image)), gbc);
+                    if(x==5){y++; x=0;}
+                    else{x++;}
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        setResizable(false);
+        add(new Panel());
+        setVisible(true);
     }
 
     public void movePawn(Object j){}
@@ -46,4 +87,34 @@ public class GameBoardGraphic extends JFrame{
     public void winGraphics(){}
 
     public void loseGraphics(){}
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        Component a = findComponentAt(e.getPoint());
+        int griX = GridBagLayoutgrid.getConstraints(a).gridx;
+        int griY = GridBagLayoutgrid.getConstraints(a).gridy;
+        System.out.println("(" + griX + ", " + griY + ")");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
