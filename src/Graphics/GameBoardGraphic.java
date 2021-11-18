97a14e83c8cb4c12bc8da501a9a1e32f3002a1ce
Tomin <tchazhikat@gmail.com>
2021-11-18 17:19:51 -0600

    Updated code based on commits 11/18

--- src/Graphics/GameBoardGraphic.java
+++ src/Graphics/GameBoardGraphic.java
@@ -1,6 +1,7 @@
 package Graphics;
 
 import Logic.GameState;
+import Logic.Pawn;
 import Logic.Tile;
 
 import javax.swing.*;
@@ -10,45 +11,63 @@ import java.awt.event.MouseListener;
 import java.util.HashMap;
 
 public class GameBoardGraphic extends JFrame implements MouseListener {
-    private static final int WIDTH = 1350;
-    private static final int HEIGHT = 750;
+    private static final int WIDTH = 2560;
+    private static final int HEIGHT = 1440;
     private Font Font;
     private JButton nextTurn;
     private JButton move;
-    private JPanel gameBoard;
+    private JPanel gameTiles;
+    private JPanel playerDeckView;
     private JPanel heliPanel;
     private JPanel specialAbility;
-    private Color limeGreen;
     private final GridBagLayout GridBagLayoutgrid;
     private GridBagConstraints gbc;
     private HashMap<int[], Tile> localTileLoc;
 
     public GameBoardGraphic(){
         Container win = getContentPane();
-        win.setLayout(null);
+        win.setLayout(new GridBagLayout());
         addMouseListener(this);
 
-        localTileLoc = new HashMap<>();
+        GridBagConstraints frameGbc = new GridBagConstraints();
+        frameGbc.gridx = 0;
+        frameGbc.gridy = 0;
+        playerDeckView =  new JPanel(){
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for(Pawn p: GameState.pawnLoc){
+                    for(int i=0; i<p.getHand().size(); i++){
+                        for(int a=100; a<500; a+=100){
+                            g.drawImage(p.getHand().get(i).getImage(), (i+1)*100, a, null);
+                        }
+                    }
+                }
+            }
+        };
+        this.add(playerDeckView, frameGbc);
 
+        localTileLoc = new HashMap<>();
 
 
         GridBagLayoutgrid = new GridBagLayout();
         gbc = new GridBagConstraints();
         this.setLayout(GridBagLayoutgrid);
         gbc.fill = GridBagConstraints.HORIZONTAL;
-//        GridBagLayout layout = new GridBagLayout();
-//        this.setLayout(layout);
+        gameTiles = new JPanel();
+        gameTiles.setLayout(GridBagLayoutgrid);
+        frameGbc.gridx = 1;
+        frameGbc.gridy = 1;
+        add(gameTiles, frameGbc);
 
         int x = 2; int y=0; int i =0;
-        for(Tile t: GameState.tileLoc.keySet()){
+        for(Tile t: GameState.tileLoc){
             gbc.gridx = x;
             gbc.gridy = y;
             int[] loc = {x,y};
-            System.out.println("(" + x + ", " + y + ")");
-            GameState.tileLoc.put(t, loc);
+            t.setLocation(x,y);
             localTileLoc.put(loc, t);
             Image image = t.getImage().getScaledInstance(100, 100,  Image.SCALE_SMOOTH); // transform it
-            this.add(new JLabel(new ImageIcon(image)), gbc);
+            gameTiles.add(new JLabel(new ImageIcon(image)), gbc);
             if(i == 0|| i == 5){
                 if(x == 3){
                     y++;
@@ -110,7 +129,7 @@ public class GameBoardGraphic extends JFrame implements MouseListener {
         setVisible(true);
     }
 
-    public void movePawn(Object j){}
+    public void movePawn(){}
 
     public void tradeable(){}
 
