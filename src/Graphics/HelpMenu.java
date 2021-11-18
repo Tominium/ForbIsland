c29255816daa364bc49e4dfcd38075138f59d841
slhscs296 <tchazhikat@gmail.com>
2021-11-18 12:18:53 -0600

    Day something

--- src/Graphics/HelpMenu.java
+++ src/Graphics/HelpMenu.java
@@ -16,26 +16,18 @@ public class HelpMenu extends JFrame {
         super("Help Menu");
 
         Container win = getContentPane();
-        win.setLayout(null);
-        helpMenu = new JFrame();
-        menuBar = new JMenuBar();
-        helpTitle = new JLabel("Help Menu");
-        JMenu menuFile = new JMenu("File");
-        JMenuItem menuItemExit = new JMenuItem("Exit");
         JLabel heading = new JLabel("Help Menu");
         heading.setSize(750, 60);
         heading.setFont(new Font("Calibri", Font.BOLD, 50));
-        heading.setForeground(new Color(250, 100, 127));
+        heading.setForeground(new Color(0, 0, 0));
         heading.setHorizontalAlignment(JLabel.CENTER);
         heading.setVerticalAlignment(JLabel.CENTER);
         win.add(heading);
         helpMenu.getContentPane().add(heading);
-        helpMenu.add(menuFile);
-        helpMenu.setJMenuBar(menuBar);
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(HIDE_ON_CLOSE);
         setResizable(false);
-        //add(new Panel());
+        add(new Panel());
         setVisible(true);
     }
 
--- src/Logic/GameState.java
+++ src/Logic/GameState.java
@@ -99,4 +99,6 @@ public class GameState {
 //        return false;
 //    }
 
+    
+
 }
