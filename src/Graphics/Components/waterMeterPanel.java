d4f312f1817644323c6abbc8dfeafa7e21bea984
Tomin <tchazhikat@gmail.com>
2021-11-25 12:53:00 -0600

    Class that extends JPanel. Component to store graphics for waterMeter

---
+++ src/Graphics/Components/waterMeterPanel.java
@@ -0,0 +1,21 @@
+package Graphics.Components;
+
+import Logic.GameState;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class waterMeterPanel extends JPanel {
+
+    public waterMeterPanel(){
+        JLabel label = new JLabel(new ImageIcon(GameState.waterMeter.getMeter()));
+        label.setBorder(new EmptyBorder(100, 65, 0 ,0));
+        add(label);
+    }
+
+    public void update(){
+        JLabel label = new JLabel(new ImageIcon(GameState.waterMeter.getMeter()));
+        label.setBorder(new EmptyBorder(100, 65, 0 ,0));
+        add(label);
+    }
+}
