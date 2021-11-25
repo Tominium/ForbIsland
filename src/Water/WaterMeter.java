354311d2332b008ddb0ef1c22ee2ad6083fe7a80
Tomin <tchazhikat@gmail.com>
2021-11-25 12:52:25 -0600

    Added functionality to store and update image of water meter

--- src/Water/WaterMeter.java
+++ src/Water/WaterMeter.java
@@ -1,13 +1,23 @@
 package Water;
 
-import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class WaterMeter {
-    private BufferedImage meter;
+    private Image meter;
     private double waterLevel;
 
     public WaterMeter(double level){
         waterLevel = level;
+        String url = "src/Assets/WaterMeterParts/" + waterLevel + ".png";
+        try{
+            meter = ImageIO.read(new File(url)).getScaledInstance(221, 669,  Image.SCALE_SMOOTH);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean hasLostGame(){
@@ -19,10 +29,25 @@ public class WaterMeter {
 
     public double watersRise(){
         waterLevel += 0.25;
+        updateMeter();
         return waterLevel;
     }
 
     public double getWaterLevel(){
         return waterLevel;
+    }
+
+    private void updateMeter(){
+        String url = "src/Assets/WaterMeterParts/" + waterLevel + ".png";
+        try{
+            meter = ImageIO.read(new File(url)).getScaledInstance(221, 669,  Image.SCALE_SMOOTH);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Image getMeter(){
+        return meter;
     }
 }
