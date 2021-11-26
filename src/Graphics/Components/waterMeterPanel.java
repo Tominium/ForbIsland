afb1dcb075c2307cc77a5e86a85bebce797bdc71
Tomin <tchazhikat@gmail.com>
2021-11-25 19:27:54 -0600

    Played around with formatting

--- src/Graphics/Components/waterMeterPanel.java
+++ src/Graphics/Components/waterMeterPanel.java
@@ -9,13 +9,13 @@ public class waterMeterPanel extends JPanel {
 
     public waterMeterPanel(){
         JLabel label = new JLabel(new ImageIcon(GameState.waterMeter.getMeter()));
-        label.setBorder(new EmptyBorder(100, 65, 0 ,0));
+        label.setBorder(new EmptyBorder(100, 35, 0 ,0));
         add(label);
     }
 
     public void update(){
         JLabel label = new JLabel(new ImageIcon(GameState.waterMeter.getMeter()));
-        label.setBorder(new EmptyBorder(100, 65, 0 ,0));
+        label.setBorder(new EmptyBorder(100, 35, 0 ,0));
         add(label);
     }
 }
