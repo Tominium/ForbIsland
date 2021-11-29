8ef7149b747bb9599dca7a0b1daad151e8fa7138
Tomin <tchazhikat@gmail.com>
2021-11-28 20:27:11 -0600

    Removed class redundancy caused by Component.GameBoard

--- src/Graphics/GameBoardGraphic.java
+++ src/Graphics/GameBoardGraphic.java
@@ -1,25 +1,22 @@
 package Graphics;
 
+import Graphics.Components.GameBoard;
 import Graphics.Components.playerDeckPanel;
 import Graphics.Components.waterMeterPanel;
 import Logic.GameState;
-import Logic.Pawn;
 import Logic.Tile;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
 public class GameBoardGraphic extends JFrame implements MouseListener {
     private static final int WIDTH = 1600;
     private static final int HEIGHT = 1082 ;
     private Font Font;
-    private JPanel gameTiles;
-    private final GridBagLayout GridBagLayoutgrid;
-    private GridBagConstraints gbc;
+    private GameBoard gameTiles;
     private playerDeckPanel playerDeckView;
     private waterMeterPanel waterMeter;
     private JPanel heliPanel;
@@ -40,12 +37,7 @@ public class GameBoardGraphic extends JFrame implements MouseListener {
 
         mainComps.add(playerDeckView);
 
-        gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gameTiles = new JPanel();
-        GridBagLayoutgrid = new GridBagLayout();
-        gameTiles.setLayout(GridBagLayoutgrid);
-        paintTile();
+        gameTiles = new GameBoard();
         mainComps.add(gameTiles);
 
         waterMeter = new waterMeterPanel();
@@ -77,90 +69,13 @@ public class GameBoardGraphic extends JFrame implements MouseListener {
         setVisible(true);
     }
 
-    private void paintTile(){
-        int x = 2; int y=0; int i =0;
-        gbc.ipadx = 2;
-        gbc.ipady = 2;
-        for(Tile t: GameState.tileLoc){
-            if(t.getName().contains("Fool")){GameState.setOriginalPlayerLocation(x, y);}
-            gbc.gridx = x;
-            gbc.gridy = y;
-            t.setLocation(x,y);
-            localTileLoc.put(new Point(x, y), t);
-            Image image = t.getImage().getScaledInstance(120, 120,  Image.SCALE_SMOOTH); // transform it
-            gameTiles.add(new JLabel(new ImageIcon(image)), gbc);
-            if(i == 0|| i == 5){
-                if(x == 3){
-                    y++;
-                    i++;
-                    x = 1;
-                }
-                else {
-                    x++;
-                }
-            }
-            else if(i == 1 || i == 4){
-                if(i == 1){
-                    if(x == 4){
-                        y++;
-                        i++;
-                        x = 0;
-                    }
-                    else{
-                        x++;
-                    }
-                }
-                else if(x == 4){
-                    y++;
-                    i++;
-                    x = 2;
-                }
-                else{
-                    x++;
-                }
-            }
-            else if(i == 2 || i == 3){
-                if(i == 2){
-                    if(x == 5){
-                        y++;
-                        i++;
-                        x = 0;
-                    }
-                    else{
-                        x++;
-                    }
-                }
-                else if(x == 5){
-                    y++;
-                    i++;
-                    x = 1;
-                }
-                else{
-                    x++;
-                }
-            }
-        }
-        gbc.ipadx = 0;
-        gbc.ipady = 0;
-        for(Pawn p: GameState.pawnLoc){
-            gbc.gridx = p.getLocation().get(0);
-            gbc.gridy = p.getLocation().get(1);
-            BufferedImage result = new BufferedImage(120, 120, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D gbi = result.createGraphics();
-            BufferedImage piece = p.getPiece();
-            gbi.drawImage(localTileLoc.get(new Point(gbc.gridx, gbc.gridy)).getImage(), 0, 0, this);
-            gbi.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 0.75f));
-            gbi.drawImage(p.getPiece(), 0, 0, this);
-            gameTiles.add(new JLabel(new ImageIcon(result)), gbc);
-        }
-
-    }
-
     public void movePawn(){}
 
     public void tradeable(){}
 
     public void removeCard(){}
+
+    public void shoreUp(){}
 
     public void sandBag(){}
 
@@ -176,10 +91,20 @@ public class GameBoardGraphic extends JFrame implements MouseListener {
     @Override
     public void mouseClicked(MouseEvent e) {
         Component a = findComponentAt(e.getPoint());
-        int griX = GridBagLayoutgrid.getConstraints(a).gridx;
-        int griY = GridBagLayoutgrid.getConstraints(a).gridy;
-        System.out.println("(" + griX + ", " + griY + ")");
-        System.out.println(GameState.check(localTileLoc.get(new Point(griX,griY)), GameState.pawnLoc.get(0)));
+        int griX = gameTiles.getGridBagLayoutgrid().getConstraints(a).gridx;
+        int griY = gameTiles.getGridBagLayoutgrid().getConstraints(a).gridy;
+        if(griX!=-1) {
+            System.out.println("(" + griX + ", " + griY + ")");
+            System.out.println(GameState.check(localTileLoc.get(new Point(griX, griY)), GameState.pawnLoc.get(0)));
+            for(int i=0; i<GameState.tileLoc.size(); i++){
+                if(GameState.tileLoc.get(i).equals(localTileLoc.get(new Point(griX, griY)))){
+                    Tile t = GameState.tileLoc.get(i); t.floodTile(); GameState.tileLoc.set(i, t);
+                    gameTiles.paintTile();
+                    mainComps.repaint();
+                    mainComps.revalidate();
+                }
+            }
+        }
     }
 
     @Override
