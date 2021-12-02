b150a360d480db3755d259aa608cbd7960c7745d
slhscs295 <siddhibavare@gmail.com>
2021-12-02 12:18:47 -0600

    made collect treasure method

--- src/Graphics/HelpMenu.java
+++ src/Graphics/HelpMenu.java
@@ -15,97 +15,44 @@ public class HelpMenu extends JFrame implements ActionListener {
         Container win = getContentPane();
         win.setLayout(null);
 
-        heading = new JLabel("Help Menu");
-        heading.setSize(750, 60);
-        heading.setFont(new Font("Impact", Font.BOLD, 50));
-        heading.setForeground(new Color(98, 98, 103, 255));
-        heading.setHorizontalAlignment(JLabel.CENTER);
-        heading.setVerticalAlignment(JLabel.CENTER);
+        heading = new JLabel("Help Menu");heading.setSize(750, 60);heading.setFont(new Font("Impact", Font.BOLD, 50));heading.setForeground(new Color(98, 98, 103, 255));heading.setHorizontalAlignment(JLabel.CENTER);heading.setVerticalAlignment(JLabel.CENTER);
         win.add(heading);
 
-        page = new JLabel();
-        page.setSize(500, 700);
-        page.setLocation(150,100);
+        page = new JLabel();page.setSize(500, 700);page.setLocation(150,100);
         win.add(page);
-
         ImageIcon icon = new ImageIcon("src/Assets/HelpPages/pOne.png");
         page.setIcon(icon);
-        back = new JButton("Back");
-        back.setSize(100,45);
-        back.setFont(new Font("Arial", Font.BOLD, 14));
-        back.setForeground(new Color(98, 98, 103,255));
-        back.setLocation(30,75);
-        back.addActionListener(this);
+
+        back = new JButton("Back");back.setSize(100,45);back.setFont(new Font("Arial", Font.BOLD, 14));back.setForeground(new Color(98, 98, 103,255));back.setLocation(30,75);back.addActionListener(this);
         win.add(back);
 
-        pOne = new JButton("1");
-        pOne.setSize(50,35);
-        pOne.setFont(new Font("Arial", Font.BOLD, 12));
-        pOne.setForeground(new Color(98, 98, 103,255));
-        pOne.setLocation(100,850);
-        pOne.addActionListener(this);
+        pOne = new JButton("1");pOne.setSize(50,35);pOne.setFont(new Font("Arial", Font.BOLD, 12));pOne.setForeground(new Color(98, 98, 103,255));pOne.setLocation(100,850);pOne.addActionListener(this);
         win.add(pOne);
 
-        pTwo= new JButton("2");
-        pTwo.setSize(50,35);
-        pTwo.setFont(new Font("Arial", Font.BOLD, 12));
-        pTwo.setForeground(new Color(98, 98, 103,255));
-        pTwo.setLocation(175,850);
-        pTwo.addActionListener(this);
+        pTwo= new JButton("2");pTwo.setSize(50,35);pTwo.setFont(new Font("Arial", Font.BOLD, 12));pTwo.setForeground(new Color(98, 98, 103,255));pTwo.setLocation(175,850);pTwo.addActionListener(this);
         win.add(pTwo);
 
-        pThree = new JButton("3");
-        pThree.setSize(50,35);
-        pThree.setFont(new Font("Arial", Font.BOLD, 12));
-        pThree.setForeground(new Color(98, 98, 103,255));
-        pThree.setLocation(250,850);
-        pThree.addActionListener(this);
+        pThree = new JButton("3");pThree.setSize(50,35);pThree.setFont(new Font("Arial", Font.BOLD, 12));pThree.setForeground(new Color(98, 98, 103,255));pThree.setLocation(250,850);pThree.addActionListener(this);
         win.add(pThree);
 
-        pFour = new JButton("4");
-        pFour.setSize(50,35);
-        pFour.setFont(new Font("Arial", Font.BOLD, 12));
-        pFour.setForeground(new Color(98, 98, 103,255));
-        pFour.setLocation(325,850);
-        pFour.addActionListener(this);
+        pFour = new JButton("4");pFour.setSize(50,35);pFour.setFont(new Font("Arial", Font.BOLD, 12));pFour.setForeground(new Color(98, 98, 103,255));pFour.setLocation(325,850);pFour.addActionListener(this);
         win.add(pFour);
 
-        pFive = new JButton("5");
-        pFive.setSize(50,35);
-        pFive.setFont(new Font("Arial", Font.BOLD, 12));
-        pFive.setForeground(new Color(98, 98, 103,255));
-        pFive.setLocation(400,850);
-        pFive.addActionListener(this);
+        pFive = new JButton("5");pFive.setSize(50,35);pFive.setFont(new Font("Arial", Font.BOLD, 12));pFive.setForeground(new Color(98, 98, 103,255));pFive.setLocation(400,850);pFive.addActionListener(this);
         win.add(pFive);
 
-        pSix = new JButton("6");
-        pSix.setSize(50,35);
-        pSix.setFont(new Font("Arial", Font.BOLD, 12));
-        pSix.setForeground(new Color(98, 98, 103,255));
-        pSix.setLocation(475,850);
-        pSix.addActionListener(this);
+        pSix = new JButton("6");pSix.setSize(50,35);pSix.setFont(new Font("Arial", Font.BOLD, 12));pSix.setForeground(new Color(98, 98, 103,255));pSix.setLocation(475,850);pSix.addActionListener(this);
         win.add(pSix);
 
-        pSeven = new JButton("7");
-        pSeven.setSize(50,35);
-        pSeven.setFont(new Font("Arial", Font.BOLD, 12));
-        pSeven.setForeground(new Color(98, 98, 103,255));
-        pSeven.setLocation(550,850);
-        pSeven.addActionListener(this);
+        pSeven = new JButton("7");pSeven.setSize(50,35);pSeven.setFont(new Font("Arial", Font.BOLD, 12));pSeven.setForeground(new Color(98, 98, 103,255));pSeven.setLocation(550,850);pSeven.addActionListener(this);
         win.add(pSeven);
 
-        pEight = new JButton("8");
-        pEight.setSize(50,35);
-        pEight.setFont(new Font("Arial", Font.BOLD, 12));
-        pEight.setForeground(new Color(98, 98, 103,255));
-        pEight.setLocation(625,850);
-        pEight.addActionListener(this);
+        pEight = new JButton("8");pEight.setSize(50,35);pEight.setFont(new Font("Arial", Font.BOLD, 12));pEight.setForeground(new Color(98, 98, 103,255));pEight.setLocation(625,850);pEight.addActionListener(this);
         win.add(pEight);
-        // buttons
+
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(HIDE_ON_CLOSE);
         setResizable(false);
-        //add(new Panel());
         setVisible(true);
     }
 
